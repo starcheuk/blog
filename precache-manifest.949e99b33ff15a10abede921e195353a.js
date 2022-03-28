c3994642cd71e1c5d401327298627d5e5917c921
Star Cheuk <183@tranthinktech.com>
2022-03-28 21:46:28 +0800

    deploy

---
+++ precache-manifest.949e99b33ff15a10abede921e195353a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cac4ea96ab39b0aab36",
+    "url": "/blog/css/app.3ec98f28.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/blog/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0874a1cea608fd6cb9487d6148cf9122",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "bd20c3b2cacb6e5cea16",
+    "url": "/blog/js/about.f3cd8948.js"
+  },
+  {
+    "revision": "7cac4ea96ab39b0aab36",
+    "url": "/blog/js/app.62e38ee5.js"
+  },
+  {
+    "revision": "7af62dbf30a400f3838d",
+    "url": "/blog/js/chunk-vendors.b07fe7fa.js"
+  },
+  {
+    "revision": "69b649f3f7f4f8f6e1b3afed797a8459",
+    "url": "/blog/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/blog/robots.txt"
+  }
+]);
